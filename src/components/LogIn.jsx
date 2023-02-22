c4929401ff41aa61b5745b5a05eb624f8baa90b3
Ashok Manjhu <125644668+dev-ashokmanjhu@users.noreply.github.com>
2023-02-22 18:39:40 +0530

    Update LogIn.jsx

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -27,6 +27,7 @@ const LogIn = ({ setIsLoggedIn }) => {
       })
 
       .catch((err) => alert(err.response.data.message));
+    setIsLoading(false)
   };
   return (
     <article className="br3 ba mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
